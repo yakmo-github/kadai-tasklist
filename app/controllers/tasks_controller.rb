a87a0a4b8bbea0365e4ca22f3beeb34975574e81
yakmo <savan.1715@gmail.com>
2020-09-10 16:54:08 +0000

    plus login authentication

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show,:edit,:update,:destroy]
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.order(id: :asc)
   end
 
   def show
@@ -8,15 +10,15 @@ class TasksController < ApplicationController
   end
 
   def new
-     @task = Task.new
+     @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
@@ -46,13 +48,20 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
 
-private
+ private
 
   # Strong Parameter
 
 
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
 
